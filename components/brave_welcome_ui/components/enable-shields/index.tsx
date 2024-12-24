4ca0c89990a673aef2762482240bb73035d7a16f
Naeem Malik <naaeemmalik21@gmail.com>
2024-12-24 17:12:24 +0500

    enable shied page added

---
+++ components/brave_welcome_ui/components/enable-shields/index.tsx
@@ -0,0 +1,113 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+
+import * as S from './style'
+import Button from '../button/index'
+import classnames from '$web-common/classnames'
+import DataContext from '../../state/context'
+// import { ViewType } from '../../state/component_types'
+// import { useViewTypeTransition } from '../../state/hooks'
+import { WelcomeBrowserProxyImpl } from '../../api/welcome_browser_proxy'
+import { getLocale } from '$web-common/locale'
+
+interface BrowserItemButtonProps {
+  browserName: string
+  onChange?: (browserName: string) => void
+  isActive: boolean
+}
+
+
+function BrowserItemButton (props: BrowserItemButtonProps) {
+  const handleClick = () => {
+    props.onChange?.(props.browserName)
+  }
+
+  const buttonClass = classnames({
+    'browser-item': true,
+    'is-selected': props.isActive
+  })
+
+  return (
+    <button onClick={handleClick}
+      className={buttonClass}
+    >
+      <i className="check-icon-box">
+        {props.isActive && (
+          <svg viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.9558.9327c-.2259-.2134-.5083-.2667-.7907-.2667s-.5648.16-.7907.3733l-7.06 7.68-3.7276-3.4666c-.4518-.4267-1.186-.4267-1.5814 0-.226.2133-.3389.48-.3389.7466 0 .2667.113.5334.3389.7467l4.5748 4.2667c.1695.2133.4519.32.7907.32h.0565c.3389 0 .6213-.16.7907-.3734l7.8507-8.5333c.3953-.4267.3389-1.12-.113-1.4933Z" fill="#4C54D2"/>
+          </svg>
+        )}
+      </i>
+      <p className="browser-name">{props.browserName}</p>
+    </button>
+  )
+}
+
+function EnableShields () {
+  const {
+    // currentSelectedBrowserProfiles,
+    // viewType,
+    currentSelectedBrowser,
+    setCurrentSelectedBrowser,
+    // setViewType,
+    // incrementCount,
+    // scenes
+  } = React.useContext(DataContext)
+  const browserTypes = ["Yes, Block Away!","No, I was looking for new sucks"]
+  const handleSelectionChange = (browserName: string) => {
+    setCurrentSelectedBrowser?.(browserName)
+  }
+
+  const handleImport = () => {
+    window.open('chrome://newtab', '_self')
+      }
+
+  React.useEffect(() => {
+    WelcomeBrowserProxyImpl.getInstance().getDefaultBrowser().then(
+      (name: string) => {
+      setCurrentSelectedBrowser?.(name)
+    })
+  }, [])
+
+  return (
+    <S.MainBox>
+      <div className="view-header-box">
+        <div className="view-details">
+          <h1 className="view-title">{getLocale('braveWelcomeEnableRewardsTitle')}</h1>
+          <p className="view-desc">{getLocale('braveWelcomeEnableRewardsDesc')}</p>
+        </div>
+      </div>
+      <div className="right-box">
+      <S.BrowserListBox>
+        <div className="browser-list">
+          {browserTypes.map((entry, id) => {
+            return (
+              <BrowserItemButton
+                key={id}
+                browserName={entry ?? 'Chromium-based browser'}
+                onChange={handleSelectionChange}
+                isActive={entry === currentSelectedBrowser}
+              />
+            )
+          })}
+        </div>
+      </S.BrowserListBox>
+        <S.ActionBox>
+          <Button
+            isPrimary={true}
+            onClick={handleImport}
+            scale="large"
+          >
+            Finish
+          </Button>
+      </S.ActionBox>
+      </div>
+    </S.MainBox>
+  )
+}
+
+export default EnableShields
---
+++ components/brave_welcome_ui/components/enable-shields/style.ts
@@ -0,0 +1,116 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at https://mozilla.org/MPL/2.0/.
+
+import styled from 'styled-components'
+
+export const MainBox = styled.div`
+  border-radius: 30px;
+  max-width: 800px;
+  color: white;
+  font-family: ${(p) => p.theme.fontFamily.heading};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .view-header-box {
+    display: grid;
+    grid-template-columns: 0.2fr 1fr 0.2fr;
+    margin-bottom: 50px;
+    text-align: center;
+    max-width: 400px;
+  }
+
+  .view-details {
+    grid-column: 2;
+  }
+
+  .view-title {
+    font-weight: 700;
+    font-size: 36px;
+    margin: 0 0 18px 0;
+    color:black;
+  }
+
+  .view-desc {
+    font-weight: 500;
+    font-size: 14px;
+    margin: 0;
+    color:black;
+  }
+
+  .right-box{
+  display: flex;
+  flex-direction: column;
+  margin:40px 20px 0 0;
+}
+`
+
+export const BrowserListBox = styled.div`
+  max-width:400px;
+  .browser-list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    grid-gap: 10px;
+    flex-direction: column;
+    flex-wrap: wrap;
+
+  }
+
+  .browser-item {
+    --border-color: transparent;
+    background: white;
+    border-radius: 10px;
+    width: 224px;
+    height: 50px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    color: #212529;
+    border: 0;
+    box-shadow: 0 0 0 4px var(--border-color);
+    position: relative;
+
+    &.is-selected {
+      --border-color: #737ADE;
+    }
+  }
+
+  .browser-name {
+    font-weight: 600;
+    font-size: 12px;
+    margin: 0;
+  }
+
+  .browser-logo-box {
+    width: 35px;
+    height: 40px;
+  }
+
+  .check-icon-box {
+    width: 16px;
+    height: 16px;
+    margin-right:5px;
+  }
+`
+
+export const ActionBox = styled.div`
+  display: flex;
+  grid-gap: 10px;
+  flex-direction: column;
+  margin-top:15px;
+  button {
+    color: white;
+  }
+
+  button:nth-child(2) {
+    color:black;
+    &[disabled] {
+      background: rgba(255, 255, 255, 0.14);
+      backdrop-filter: blur(8px);
+      color: rgba(255, 255, 255, 0.32);
+    }
+  }
+`
