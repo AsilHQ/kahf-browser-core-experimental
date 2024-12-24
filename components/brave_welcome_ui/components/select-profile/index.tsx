e7be42772400161f35747c1f9336a3f1db519613
Naeem Malik <naaeemmalik21@gmail.com>
2024-12-24 17:03:12 +0500

    profile select ui page updated

--- components/brave_welcome_ui/components/select-profile/index.tsx
+++ components/brave_welcome_ui/components/select-profile/index.tsx
@@ -6,31 +6,89 @@
 import * as React from 'react'
 
 import * as S from './style'
-import Button from '$web-components/button'
-import LeftArrowSVG from '../svg/left-arrow'
-import AvatarIconSVG from '../svg/avatar-icon'
+import Button from '../button/index'
+import classnames from '$web-common/classnames'
 import DataContext from '../../state/context'
-import { useViewTypeTransition } from '../../state/hooks'
+// import { ViewType } from '../../state/component_types'
+import { getUniqueBrowserTypes } from '../../state/utils'
+// import { useViewTypeTransition } from '../../state/hooks'
 import { WelcomeBrowserProxyImpl, ImportDataBrowserProxyImpl, defaultImportTypes, P3APhase } from '../../api/welcome_browser_proxy'
 import { getLocale } from '$web-common/locale'
 
+import ChromeCanarySVG from '../svg/browser-icons/chrome-canary'
+import ChromeSVG from '../svg/browser-icons/chrome'
+import ChromeBetaSVG from '../svg/browser-icons/chrome-beta'
+import ChromeDevSVG from '../svg/browser-icons/chrome-dev'
+import ChromiumSVG from '../svg/browser-icons/chromium'
+import EdgeSVG from '../svg/browser-icons/edge'
+import FirefoxSVG from '../svg/browser-icons/firefox'
+import OperaSVG from '../svg/browser-icons/opera'
+import SafariSVG from '../svg/browser-icons/safari'
+import VivaldiSVG from '../svg/browser-icons/vivaldi'
+import WhaleSVG from '../svg/browser-icons/whale'
+import YandexSVG from '../svg/browser-icons/yandex'
+import MicrosoftIE from '../svg/browser-icons/ie'
+// import AvatarIconSVG from '../svg/avatar-icon'
+
+interface BrowserItemButtonProps {
+  browserName: string
+  onChange?: (browserName: string) => void
+  isActive: boolean
+}
 interface ProfileItemProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   profileName: string
   id: number
   isChecked: boolean
 }
+const browserIcons = {
+  'Google Chrome Canary': <ChromeCanarySVG />,
+  'Google Chrome': <ChromeSVG />,
+  'Google Chrome Dev': <ChromeDevSVG />,
+  'Google Chrome Beta': <ChromeBetaSVG />,
+  'Chromium': <ChromiumSVG />,
+  'Microsoft Edge': <EdgeSVG />,
+  'Firefox': <FirefoxSVG />,
+  'Opera': <OperaSVG />,
+  'Safari': <SafariSVG />,
+  'Vivaldi': <VivaldiSVG />,
+  'NAVER Whale': <WhaleSVG />,
+  'Yandex': <YandexSVG />,
+  'Microsoft Internet Explorer': <MicrosoftIE />
+}
 
+function BrowserItemButton (props: BrowserItemButtonProps) {
+  const handleClick = () => {
+    props.onChange?.(props.browserName)
+  }
+
+  const buttonClass = classnames({
+    'browser-item': true,
+    'is-selected': props.isActive
+  })
+
+  return (
+    <button onClick={handleClick}
+      className={buttonClass}
+    >
+      <i className="check-icon-box">
+        {props.isActive && (
+          <svg viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.9558.9327c-.2259-.2134-.5083-.2667-.7907-.2667s-.5648.16-.7907.3733l-7.06 7.68-3.7276-3.4666c-.4518-.4267-1.186-.4267-1.5814 0-.226.2133-.3389.48-.3389.7466 0 .2667.113.5334.3389.7467l4.5748 4.2667c.1695.2133.4519.32.7907.32h.0565c.3389 0 .6213-.16.7907-.3734l7.8507-8.5333c.3953-.4267.3389-1.12-.113-1.4933Z" fill="#4C54D2"/>
+          </svg>
+        )}
+      </i>
+      <div className="browser-logo-box">
+        {browserIcons[props.browserName]}
+      </div>
+      <p className="browser-name">{props.browserName}</p>
+    </button>
+  )
+}
 function ProfileItem (props: ProfileItemProps) {
   return (
     <div className="item-box">
-      <label className="item-grid">
-        <div className="item-info">
-          <div className="avatar">
-            <AvatarIconSVG />
-          </div>
-          <span>{props.profileName}</span>
-        </div>
+        <label className="item-grid">
         <div className="item-action">
           <input
             type="checkbox"
@@ -39,42 +97,53 @@ function ProfileItem (props: ProfileItemProps) {
             onChange={props.onChange}
           />
         </div>
+        <div className="item-info">
+          <span>{props.profileName}</span>
+        </div>
+        
       </label>
     </div>
   )
 }
-
 function SelectProfile () {
   const {
-    viewType,
-    setViewType,
+    browserProfiles,
+    currentSelectedBrowserProfiles,
+    // viewType,
+    currentSelectedBrowser,
+    setCurrentSelectedBrowser,
+    // setViewType,
     incrementCount,
-    currentSelectedBrowserProfiles
+    // scenes
   } = React.useContext(DataContext)
+  const browserTypes = getUniqueBrowserTypes(browserProfiles ?? [])
+  const handleSelectionChange = (browserName: string) => {
+    setCurrentSelectedBrowser?.(browserName)
+  }
   const [selectedProfiles, setSelectedProfiles] = React.useState<Set<number>>(new Set())
+//   const { forward, skip } = useViewTypeTransition(viewType)
 
-  const { back } = useViewTypeTransition(viewType)
-  const handleBackButton = () => setViewType(back!)
+//   const handleImport = () => {
+//     if (!currentSelectedBrowser || !currentSelectedBrowserProfiles) return
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, checked } = e.target
-    const parsedId = parseInt(id.split('-')[1])
+//     if (forward === ViewType.ImportSelectProfile) {
+//       setViewType(ViewType.ImportSelectProfile)
+//       WelcomeBrowserProxyImpl.getInstance().recordP3A(P3APhase.Import)
+//       return
+//     }
 
-    if (!checked && selectedProfiles?.has(parsedId)) {
-      selectedProfiles.delete(parsedId)
-      setSelectedProfiles(new Set([...selectedProfiles]))
-      return
-    }
+//     if (forward === ViewType.ImportInProgress) {
+//       ImportDataBrowserProxyImpl.getInstance().importData(
+//         currentSelectedBrowserProfiles[0].index,
+//         defaultImportTypes
+//       )
+//       incrementCount()
+//       WelcomeBrowserProxyImpl.getInstance().recordP3A(P3APhase.Consent)
+//       return
+//     }
 
-    setSelectedProfiles(new Set(selectedProfiles.add(parsedId)))
-  }
-
-  const selectAll = () => {
-    setSelectedProfiles(
-      new Set(currentSelectedBrowserProfiles?.map((profile) => profile.index))
-    )
-  }
-
+//     console.error(`Invalid forward view: ${forward}`)
+//   }
   const handleImportProfiles = () => {
     if (selectedProfiles.size <= 0) return
     let entries: number[] = []
@@ -90,34 +159,58 @@ function SelectProfile () {
     }
     WelcomeBrowserProxyImpl.getInstance().recordP3A(P3APhase.Consent)
   }
+
+  React.useEffect(() => {
+    WelcomeBrowserProxyImpl.getInstance().getDefaultBrowser().then(
+      (name: string) => {
+      setCurrentSelectedBrowser?.(name)
+    })
+  }, [])
+
+//   const hasSelectedBrowser =
+//     currentSelectedBrowserProfiles && currentSelectedBrowserProfiles.length > 0
   const getImportEntryName = (entry: any) => {
     let name = entry.name
     if (entry.profileName) {
       name += ' - ' + entry.profileName
     }
     return name
+    }
+    
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, checked } = e.target
+    const parsedId = parseInt(id.split('-')[1])
+
+    if (!checked && selectedProfiles?.has(parsedId)) {
+      selectedProfiles.delete(parsedId)
+      setSelectedProfiles(new Set([...selectedProfiles]))
+      return
+    }
+
+    setSelectedProfiles(new Set(selectedProfiles.add(parsedId)))
   }
   return (
     <S.MainBox>
       <div className="view-header-box">
-        <div className="view-header-actions">
-          <button className="back-button" onClick={handleBackButton}>
-            <LeftArrowSVG />
-            <span>{getLocale('braveWelcomeBackButtonLabel')}</span>
-          </button>
-        </div>
         <div className="view-details">
-          <h1 className="view-title">{getLocale('braveWelcomeSelectProfileLabel')}</h1>
-          <p className="view-desc">{getLocale('braveWelcomeSelectProfileDesc')}</p>
+          <h1 className="view-title">{getLocale('braveWelcomeImportSettingsTitle')}</h1>
+          <p className="view-desc">{getLocale('braveWelcomeImportSettingsDesc')}</p>
         </div>
       </div>
-      <S.ProfileListBox>
+      <div className="right-box">
+      <S.BrowserListBox>
+        <div className="browser-list">
+          {browserTypes.map((entry, id) => {
+            return (<>
+              <BrowserItemButton
+                key={id}
+                browserName={entry ?? 'Chromium-based browser'}
+                onChange={handleSelectionChange}
+                isActive={entry === currentSelectedBrowser}
+                />
+                {entry === currentSelectedBrowser && (
+                    <S.ProfileListBox>
         <div className="profile-list">
-          <div className="list-actions">
-            <div className="right">
-              <button onClick={selectAll}>{getLocale('braveWelcomeSelectAllButtonLabel')}</button>
-            </div>
-          </div>
           {currentSelectedBrowserProfiles?.map((entry) => {
             return (<ProfileItem
               key={entry.index}
@@ -128,8 +221,13 @@ function SelectProfile () {
             />)
           })}
         </div>
-      </S.ProfileListBox>
-      <S.ActionBox>
+                    </S.ProfileListBox>)}
+                </>
+            )
+          })}
+        </div>
+      </S.BrowserListBox>
+           <S.ActionBox>
         <Button
           isPrimary={true}
           onClick={handleImportProfiles}
@@ -138,6 +236,7 @@ function SelectProfile () {
           {getLocale('braveWelcomeImportProfilesButtonLabel')}
         </Button>
       </S.ActionBox>
+      </div>
     </S.MainBox>
   )
 }
--- components/brave_welcome_ui/components/select-profile/style.ts
+++ components/brave_welcome_ui/components/select-profile/style.ts
@@ -5,7 +5,7 @@
 
 import styled from 'styled-components'
 
-export const MainBox = styled.div`
+export const MainBox1 = styled.div`
   background: rgba(255, 255, 255, 0.1);
   backdrop-filter: blur(15px);
   border-radius: 30px;
@@ -64,17 +64,16 @@ export const MainBox = styled.div`
 
 export const ProfileListBox = styled.div`
   color: #212529;
-  font-size: 16px;
+  font-size: 12px;
   font-weight: 400;
-  margin-bottom: 55px;
+  margin-bottom: 5px;
 
   p {
     margin: 0;
   }
 
   .profile-list {
-    background: white;
-    min-width: 520px;
+    min-width: 200px;
     max-height: 420px;
     padding-bottom: 20px;
     border-radius: 16px;
@@ -100,17 +99,12 @@ export const ProfileListBox = styled.div`
 
   .item-box {
     background: transparent;
-
-    &:hover {
-      background: #F8F9FA;
-    }
   }
 
   .item-grid {
     display: grid;
-    grid-template-columns: 1fr 25px;
-    padding: 10px 32px;
-    align-items: center;
+    grid-template-columns: 25px 1fr;
+    padding: 10px 3px;
   }
 
   .item-info {
@@ -120,7 +114,6 @@ export const ProfileListBox = styled.div`
   }
 
   .item-action {
-    text-align: right;
   }
 
   .avatar {
@@ -135,8 +128,8 @@ export const ProfileListBox = styled.div`
   }
 
   input[type="checkbox"] {
-    width: 22px;
-    height: 22px;
+    width: 14px;
+    height: 14px;
     border-radius: 4px;
     border: 1px solid #AEB1C2;
 
@@ -146,8 +139,124 @@ export const ProfileListBox = styled.div`
   }
 `
 
+// export const ActionBox = styled.div`
+//   display: flex;
+//   grid-gap: 10px;
+//   margin-bottom: 40px;
+// `
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+export const MainBox = styled.div`
+  border-radius: 30px;
+  max-width: 800px;
+  color: white;
+  font-family: ${(p) => p.theme.fontFamily.heading};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .view-header-box {
+    display: grid;
+    grid-template-columns: 0.2fr 1fr 0.2fr;
+    margin-bottom: 50px;
+    text-align: center;
+    max-width: 400px;
+  }
+
+  .view-details {
+    grid-column: 2;
+  }
+
+  .view-title {
+    font-weight: 700;
+    font-size: 36px;
+    margin: 0 0 18px 0;
+    color:black;
+  }
+
+  .view-desc {
+    font-weight: 500;
+    font-size: 14px;
+    margin: 0;
+    color:black;
+  }
+
+  .right-box{
+  display: flex;
+  flex-direction: column;
+  margin:40px 20px 0 0;
+}
+`
+
+export const BrowserListBox = styled.div`
+  max-width:400px;
+  .browser-list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    grid-gap: 10px;
+    flex-direction: column;
+    flex-wrap: wrap;
+
+  }
+
+  .browser-item {
+    --border-color: transparent;
+    background: white;
+    border-radius: 10px;
+    width: 224px;
+    height: 50px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    color: #212529;
+    border: 0;
+    box-shadow: 0 0 0 4px var(--border-color);
+    position: relative;
+
+    &.is-selected {
+      --border-color: #737ADE;
+    }
+  }
+
+  .browser-name {
+    font-weight: 600;
+    font-size: 12px;
+    margin: 0;
+  }
+
+  .browser-logo-box {
+    width: 35px;
+    height: 40px;
+  }
+
+  .check-icon-box {
+    width: 16px;
+    height: 16px;
+    position: absolute;
+    right: 14px;
+  }
+`
+
 export const ActionBox = styled.div`
   display: flex;
   grid-gap: 10px;
-  margin-bottom: 40px;
+  flex-direction: column;
+  margin-top:15px;
+  button {
+    color: white;
+  }
+
+  button:nth-child(2) {
+    color:black;
+    &[disabled] {
+      background: rgba(255, 255, 255, 0.14);
+      backdrop-filter: blur(8px);
+      color: rgba(255, 255, 255, 0.32);
+    }
+  }
 `
