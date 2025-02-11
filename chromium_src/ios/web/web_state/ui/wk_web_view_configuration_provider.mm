eafa91412f83b945e12e3e47d7f2ded0e5ae7641
Kyle Hickinson <git@kylehickinson.com>
2025-02-11 17:49:38 -0500

    [wip] use subclass for WKWebViewConfigurationProvider patch

--- chromium_src/ios/web/web_state/ui/wk_web_view_configuration_provider.mm
+++ chromium_src/ios/web/web_state/ui/wk_web_view_configuration_provider.mm
@@ -3,36 +3,34 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#import <Foundation/Foundation.h>
-#import <WebKit/WebKit.h>
+#include "src/ios/web/web_state/ui/wk_web_view_configuration_provider.mm"
 
-#include "base/notreached.h"
+namespace web {
 
-#define BRAVE_RESET_WITH_WEB_VIEW_CONFIGURATION \
-  brave::ResetWithWebViewConfiguration(configuration_);
+void WKWebViewConfigurationProvider::ResetWithWebViewConfiguration_Unused(
+    WKWebViewConfiguration* configuration) {}
 
-namespace brave {
+void BraveWKWebViewConfigurationProvider::ResetWithWebViewConfiguration(
+    WKWebViewConfiguration* configuration) {
+  WKWebViewConfigurationProvider::ResetWithWebViewConfiguration(configuration);
 
-void ResetWithWebViewConfiguration(WKWebViewConfiguration* configuration) {
   // Adjusts the underlying WKWebViewConfiguration for settings we don't want
   // to inherit from Chromium
 
   // Restore WKWebView long press
   @try {
-    [configuration setValue:@YES forKey:@"longPressActionsEnabled"];
+    [configuration_ setValue:@YES forKey:@"longPressActionsEnabled"];
   } @catch (NSException* exception) {
     NOTREACHED() << "Error setting value for longPressActionsEnabled";
   }
 
   // Restore Apple's safe browsing implementation
-  [[configuration preferences] setFraudulentWebsiteWarningEnabled:YES];
+  [[configuration_ preferences] setFraudulentWebsiteWarningEnabled:YES];
 
   // Reset fullscreen to default as it wasn't set in Brave
-  [[configuration preferences] setElementFullscreenEnabled:NO];
+  [[configuration_ preferences] setElementFullscreenEnabled:NO];
 }
 
-}  // namespace brave
-
-#include "src/ios/web/web_state/ui/wk_web_view_configuration_provider.mm"
+}  // namespace web
 
 #undef BRAVE_RESET_WITH_WEB_VIEW_CONFIGURATION
