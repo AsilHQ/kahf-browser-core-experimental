TheKingLucifer666/brave-core

[iOS] Prepare CWVWebView web embedder for use in iOS

ios/app/brave_core_main.mm
@@ -8,6 +8,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#include <memory>
+
 #include "base/apple/bundle_locations.h"
 #include "base/apple/foundation_util.h"
 #include "base/at_exit.h"
@@ -32,6 +34,8 @@
 #include "brave/ios/browser/api/brave_shields/adblock_service+private.h"
 #include "brave/ios/browser/api/brave_stats/brave_stats+private.h"
 #include "brave/ios/browser/api/brave_wallet/brave_wallet_api+private.h"
+#include "brave/ios/browser/api/content_settings/default_host_content_settings.h"
+#include "brave/ios/browser/api/content_settings/default_host_content_settings_internal.h"
 #include "brave/ios/browser/api/de_amp/de_amp_prefs+private.h"
 #include "brave/ios/browser/api/history/brave_history_api+private.h"
 #include "brave/ios/browser/api/https_upgrade_exceptions/https_upgrade_exceptions_service+private.h"
@@ -45,9 +49,10 @@
 #include "brave/ios/browser/api/sync/brave_sync_api+private.h"
 #include "brave/ios/browser/api/sync/driver/brave_sync_profile_service+private.h"
 #include "brave/ios/browser/api/web_image/web_image+private.h"
-#include "brave/ios/browser/brave_web_client.h"
 #include "brave/ios/browser/ui/webui/brave_web_ui_controller_factory.h"
+#include "brave/ios/browser/web/brave_web_client.h"
 #include "components/component_updater/component_updater_paths.h"
+#include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/password_manager/core/browser/password_store/password_store.h"
@@ -58,6 +63,9 @@
 #include "ios/chrome/app/startup/provider_registration.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_undo_service_factory.h"
+#include "ios/chrome/browser/browsing_data/model/browsing_data_remover.h"
+#include "ios/chrome/browser/browsing_data/model/browsing_data_remover_factory.h"
+#include "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
 #include "ios/chrome/browser/credential_provider/model/credential_provider_buildflags.h"
 #include "ios/chrome/browser/history/model/history_service_factory.h"
 #include "ios/chrome/browser/history/model/web_history_service_factory.h"
@@ -78,6 +86,11 @@
 #include "ios/public/provider/chrome/browser/overrides/overrides_api.h"
 #include "ios/public/provider/chrome/browser/ui_utils/ui_utils_api.h"
 #include "ios/web/public/init/web_main.h"
+#include "ios/web/public/thread/web_task_traits.h"
+#include "ios/web/public/thread/web_thread.h"
+#include "ios/web_view/internal/cwv_web_view_configuration_internal.h"
+#include "ios/web_view/internal/web_view_browser_state.h"
+#include "ios/web_view/internal/web_view_download_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
@@ -108,6 +121,8 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
   std::unique_ptr<web::WebMain> _webMain;
   std::unique_ptr<Browser> _browser;
   std::unique_ptr<Browser> _otr_browser;
+  std::unique_ptr<ios_web_view::WebViewDownloadManager> _downloadManager;
+  std::unique_ptr<ios_web_view::WebViewDownloadManager> _otrDownloadManager;
   raw_ptr<BrowserList> _browserList;
   raw_ptr<BrowserList> _otr_browserList;
   raw_ptr<ProfileIOS> _main_profile;
@@ -130,6 +145,9 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
 @property(nonatomic) NTPBackgroundImagesService* backgroundImagesService;
 @property(nonatomic)
     HTTPSUpgradeExceptionsService* httpsUpgradeExceptionsService;
+@property(nonatomic) DefaultHostContentSettings* defaultHostContentSettings;
+@property(nonatomic) CWVWebViewConfiguration* defaultWebViewConfiguration;
+@property(nonatomic) CWVWebViewConfiguration* nonPersistentWebViewConfiguration;
 @end
 
 @implementation BraveCoreMain
@@ -176,7 +194,9 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
 
     // Setup WebClient ([ClientRegistration registerClients])
     _webClient.reset(new BraveWebClient());
-    _webClient->SetUserAgent(base::SysNSStringToUTF8(userAgent));
+    if (userAgent != nil) {
+      _webClient->SetLegacyUserAgent(base::SysNSStringToUTF8(userAgent));
+    }
     web::SetWebClient(_webClient.get());
 
     _delegate.reset(new BraveMainDelegate());
@@ -234,6 +254,13 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
     _otr_browser = Browser::Create(otr_last_used_profile, {});
     _otr_browserList->AddBrowser(_otr_browser.get());
 
+    // Setup download managers for CWVWebView
+    _downloadManager =
+        std::make_unique<ios_web_view::WebViewDownloadManager>(_main_profile);
+    _otrDownloadManager =
+        std::make_unique<ios_web_view::WebViewDownloadManager>(
+            otr_last_used_profile);
+
     // Initialize the provider UI global state.
     ios::provider::InitializeUI();
 
@@ -275,6 +302,15 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
   _syncAPI = nil;
   _tabGeneratorAPI = nil;
   _webImageDownloader = nil;
+
+  [_nonPersistentWebViewConfiguration shutDown];
+  [_defaultWebViewConfiguration shutDown];
+
+  _nonPersistentWebViewConfiguration = nil;
+  _defaultWebViewConfiguration = nil;
+
+  _downloadManager.reset();
+  _otrDownloadManager.reset();
 
   _otr_browserList =
       BrowserListFactory::GetForProfile(_otr_browser->GetProfile());
@@ -561,5 +597,113 @@ static bool CustomLogHandler(int severity,
                                 /*fallback_to_google_server=*/false));
 }
 #endif
+
+- (DefaultHostContentSettings*)defaultHostContentSettings {
+  if (!_defaultHostContentSettings) {
+    HostContentSettingsMap* map =
+        ios::HostContentSettingsMapFactory::GetForProfile(_main_profile);
+    _defaultHostContentSettings =
+        [[DefaultHostContentSettings alloc] initWithSettingsMap:map];
+  }
+  return _defaultHostContentSettings;
+}
+
+- (CWVWebViewConfiguration*)defaultWebViewConfiguration {
+  if (!_defaultWebViewConfiguration) {
+    _defaultWebViewConfiguration = [[CWVWebViewConfiguration alloc]
+        initWithBrowserState:ios_web_view::WebViewBrowserState::
+                                 FromBrowserState(_main_profile)];
+  }
+  return _defaultWebViewConfiguration;
+}
+
+- (CWVWebViewConfiguration*)nonPersistentWebViewConfiguration {
+  if (!_nonPersistentWebViewConfiguration) {
+    _nonPersistentWebViewConfiguration = [[CWVWebViewConfiguration alloc]
+        initWithBrowserState:ios_web_view::WebViewBrowserState::
+                                 FromBrowserState(
+                                     _main_profile->GetOffTheRecordProfile())];
+  }
+  return _nonPersistentWebViewConfiguration;
+}
+
+#pragma mark - Handling of destroying the incognito BrowserState
+
+// The incognito BrowserState should be closed when the last incognito tab is
+// closed (i.e. if there are other incognito tabs open in another Scene, the
+// BrowserState must not be destroyed).
+- (BOOL)shouldDestroyAndRebuildIncognitoProfile {
+  return _main_profile->HasOffTheRecordProfile();
+}
+
+// Matches lastIncognitoTabClosed from Chrome's SceneController
+- (void)notifyLastPrivateTabClosed {
+  // If no other window has incognito tab, then destroy and rebuild the
+  // BrowserState. Otherwise, just do the state transition animation.
+  if ([self shouldDestroyAndRebuildIncognitoProfile]) {
+    // Incognito browser state cannot be deleted before all the requests are
+    // deleted. Queue empty task on IO thread and destroy the BrowserState
+    // when the task has executed, again verifying that no incognito tabs are
+    // present. When an incognito tab is moved between browsers, there is
+    // a point where the tab isn't attached to any web state list. However, when
+    // this queued cleanup step executes, the moved tab will be attached, so
+    // the cleanup shouldn't proceed.
+
+    auto cleanup = ^{
+      if ([self shouldDestroyAndRebuildIncognitoProfile]) {
+        [self destroyAndRebuildIncognitoProfile];
+      }
+    };
+
+    web::GetIOThreadTaskRunner({})->PostTaskAndReply(
+        FROM_HERE, base::DoNothing(), base::BindRepeating(cleanup));
+  }
+}
+
+// Matches cleanupBrowser from Chrome's BrowserViewWrangler
+- (void)cleanupBrowser:(Browser*)browser {
+  DCHECK(browser);
+
+  // Remove the Browser from the browser list. The browser itself is still
+  // alive during this call, so any observer can act on it.
+  ProfileIOS* profile = browser->GetProfile();
+  BrowserList* browserList = BrowserListFactory::GetForProfile(profile);
+  browserList->RemoveBrowser(browser);
+
+  WebStateList* webStateList = browser->GetWebStateList();
+  // Close all webstates in `webStateList`. Do this in an @autoreleasepool as
+  // WebStateList observers will be notified (they are unregistered later). As
+  // some of them may be implemented in Objective-C and unregister themselves
+  // in their -dealloc method, ensure the -autorelease introduced by ARC are
+  // processed before the WebStateList destructor is called.
+  @autoreleasepool {
+    CloseAllWebStates(*webStateList, WebStateList::CLOSE_NO_FLAGS);
+  }
+}
+
+- (void)destroyAndRebuildIncognitoProfile {
+  DCHECK(_main_profile->HasOffTheRecordProfile());
+  _nonPersistentWebViewConfiguration = nil;
+
+  ProfileIOS* otrProfile = _main_profile->GetOffTheRecordProfile();
+
+  BrowsingDataRemover* browsingDataRemover =
+      BrowsingDataRemoverFactory::GetForProfile(otrProfile);
+  browsingDataRemover->Remove(browsing_data::TimePeriod::ALL_TIME,
+                              BrowsingDataRemoveMask::REMOVE_ALL,
+                              base::DoNothing());
+
+  [self cleanupBrowser:_otr_browser.get()];
+  _otr_browser.reset();
+
+  // Destroy and recreate the off-the-record BrowserState.
+  _main_profile->DestroyOffTheRecordProfile();
+
+  otrProfile = _main_profile->GetOffTheRecordProfile();
+  _otr_browser = Browser::Create(otrProfile, {});
+
+  BrowserList* browserList = BrowserListFactory::GetForProfile(otrProfile);
+  browserList->AddBrowser(_otr_browser.get());
+}
 
 @end

AsilHQ/kahf-browser-core-experimental

ios/app/brave_core_main.mm
@@ -35,6 +35,8 @@
 #include "brave/ios/browser/api/brave_shields/adblock_service+private.h"
 #include "brave/ios/browser/api/brave_stats/brave_stats+private.h"
 #include "brave/ios/browser/api/brave_wallet/brave_wallet_api+private.h"
+#include "brave/ios/browser/api/content_settings/default_host_content_settings.h"
+#include "brave/ios/browser/api/content_settings/default_host_content_settings_internal.h"
 #include "brave/ios/browser/api/de_amp/de_amp_prefs+private.h"
 #include "brave/ios/browser/api/history/brave_history_api+private.h"
 #include "brave/ios/browser/api/https_upgrade_exceptions/https_upgrade_exceptions_service+private.h"
@@ -48,10 +50,11 @@
 #include "brave/ios/browser/api/sync/brave_sync_api+private.h"
 #include "brave/ios/browser/api/sync/driver/brave_sync_profile_service+private.h"
 #include "brave/ios/browser/api/web_image/web_image+private.h"
-#include "brave/ios/browser/brave_web_client.h"
 #include "brave/ios/browser/ui/webui/brave_web_ui_controller_factory.h"
+#include "brave/ios/browser/web/brave_web_client.h"
 #import "build/blink_buildflags.h"
 #include "components/component_updater/component_updater_paths.h"
+#include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/password_manager/core/browser/password_store/password_store.h"
@@ -62,6 +65,9 @@
 #include "ios/chrome/app/startup/provider_registration.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_undo_service_factory.h"
+#include "ios/chrome/browser/browsing_data/model/browsing_data_remover.h"
+#include "ios/chrome/browser/browsing_data/model/browsing_data_remover_factory.h"
+#include "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
 #include "ios/chrome/browser/credential_provider/model/credential_provider_buildflags.h"
 #include "ios/chrome/browser/history/model/history_service_factory.h"
 #include "ios/chrome/browser/history/model/web_history_service_factory.h"
@@ -83,6 +89,11 @@
 #include "ios/public/provider/chrome/browser/overrides/overrides_api.h"
 #include "ios/public/provider/chrome/browser/ui_utils/ui_utils_api.h"
 #include "ios/web/public/init/web_main.h"
+#include "ios/web/public/thread/web_task_traits.h"
+#include "ios/web/public/thread/web_thread.h"
+#include "ios/web_view/internal/cwv_web_view_configuration_internal.h"
+#include "ios/web_view/internal/web_view_browser_state.h"
+#include "ios/web_view/internal/web_view_download_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
@@ -130,6 +141,8 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
   std::unique_ptr<web::WebMain> _webMain;
   std::unique_ptr<Browser> _browser;
   std::unique_ptr<Browser> _otr_browser;
+  std::unique_ptr<ios_web_view::WebViewDownloadManager> _downloadManager;
+  std::unique_ptr<ios_web_view::WebViewDownloadManager> _otrDownloadManager;
   raw_ptr<BrowserList> _browserList;
   raw_ptr<BrowserList> _otr_browserList;
   raw_ptr<ProfileIOS> _main_profile;
@@ -152,6 +165,9 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
 @property(nonatomic) NTPBackgroundImagesService* backgroundImagesService;
 @property(nonatomic)
     HTTPSUpgradeExceptionsService* httpsUpgradeExceptionsService;
+@property(nonatomic) DefaultHostContentSettings* defaultHostContentSettings;
+@property(nonatomic) CWVWebViewConfiguration* defaultWebViewConfiguration;
+@property(nonatomic) CWVWebViewConfiguration* nonPersistentWebViewConfiguration;
 @end
 
 @implementation BraveCoreMain
@@ -203,7 +219,9 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
 
     // Setup WebClient ([ClientRegistration registerClients])
     _webClient.reset(new BraveWebClient());
-    _webClient->SetUserAgent(base::SysNSStringToUTF8(userAgent));
+    if (userAgent != nil) {
+      _webClient->SetLegacyUserAgent(base::SysNSStringToUTF8(userAgent));
+    }
     web::SetWebClient(_webClient.get());
 
     _delegate.reset(new BraveMainDelegate());
@@ -279,6 +297,15 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
   _tabGeneratorAPI = nil;
   _webImageDownloader = nil;
 
+  [_nonPersistentWebViewConfiguration shutDown];
+  [_defaultWebViewConfiguration shutDown];
+
+  _nonPersistentWebViewConfiguration = nil;
+  _defaultWebViewConfiguration = nil;
+
+  _downloadManager.reset();
+  _otrDownloadManager.reset();
+
   _otr_browserList =
       BrowserListFactory::GetForProfile(_otr_browser->GetProfile());
   [_otr_browser->GetCommandDispatcher() prepareForShutdown];
@@ -319,6 +346,12 @@ const BraveCoreLogSeverity BraveCoreLogSeverityVerbose =
   _otr_browserList = BrowserListFactory::GetForProfile(otr_last_used_profile);
   _otr_browser = Browser::Create(otr_last_used_profile, {});
   _otr_browserList->AddBrowser(_otr_browser.get());
+
+  // Setup download managers for CWVWebView
+  _downloadManager =
+      std::make_unique<ios_web_view::WebViewDownloadManager>(profile);
+  _otrDownloadManager = std::make_unique<ios_web_view::WebViewDownloadManager>(
+      otr_last_used_profile);
 
 #if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
   CredentialProviderServiceFactory::GetForProfile(profile);
@@ -587,5 +620,113 @@ static bool CustomLogHandler(int severity,
                                 /*fallback_to_google_server=*/false));
 }
 #endif
+
+- (DefaultHostContentSettings*)defaultHostContentSettings {
+  if (!_defaultHostContentSettings) {
+    HostContentSettingsMap* map =
+        ios::HostContentSettingsMapFactory::GetForProfile(_main_profile);
+    _defaultHostContentSettings =
+        [[DefaultHostContentSettings alloc] initWithSettingsMap:map];
+  }
+  return _defaultHostContentSettings;
+}
+
+- (CWVWebViewConfiguration*)defaultWebViewConfiguration {
+  if (!_defaultWebViewConfiguration) {
+    _defaultWebViewConfiguration = [[CWVWebViewConfiguration alloc]
+        initWithBrowserState:ios_web_view::WebViewBrowserState::
+                                 FromBrowserState(_main_profile)];
+  }
+  return _defaultWebViewConfiguration;
+}
+
+- (CWVWebViewConfiguration*)nonPersistentWebViewConfiguration {
+  if (!_nonPersistentWebViewConfiguration) {
+    _nonPersistentWebViewConfiguration = [[CWVWebViewConfiguration alloc]
+        initWithBrowserState:ios_web_view::WebViewBrowserState::
+                                 FromBrowserState(
+                                     _main_profile->GetOffTheRecordProfile())];
+  }
+  return _nonPersistentWebViewConfiguration;
+}
+
+#pragma mark - Handling of destroying the incognito BrowserState
+
+// The incognito BrowserState should be closed when the last incognito tab is
+// closed (i.e. if there are other incognito tabs open in another Scene, the
+// BrowserState must not be destroyed).
+- (BOOL)shouldDestroyAndRebuildIncognitoProfile {
+  return _main_profile->HasOffTheRecordProfile();
+}
+
+// Matches lastIncognitoTabClosed from Chrome's SceneController
+- (void)notifyLastPrivateTabClosed {
+  // If no other window has incognito tab, then destroy and rebuild the
+  // BrowserState. Otherwise, just do the state transition animation.
+  if ([self shouldDestroyAndRebuildIncognitoProfile]) {
+    // Incognito browser state cannot be deleted before all the requests are
+    // deleted. Queue empty task on IO thread and destroy the BrowserState
+    // when the task has executed, again verifying that no incognito tabs are
+    // present. When an incognito tab is moved between browsers, there is
+    // a point where the tab isn't attached to any web state list. However, when
+    // this queued cleanup step executes, the moved tab will be attached, so
+    // the cleanup shouldn't proceed.
+
+    auto cleanup = ^{
+      if ([self shouldDestroyAndRebuildIncognitoProfile]) {
+        [self destroyAndRebuildIncognitoProfile];
+      }
+    };
+
+    web::GetIOThreadTaskRunner({})->PostTaskAndReply(
+        FROM_HERE, base::DoNothing(), base::BindRepeating(cleanup));
+  }
+}
+
+// Matches cleanupBrowser from Chrome's BrowserViewWrangler
+- (void)cleanupBrowser:(Browser*)browser {
+  DCHECK(browser);
+
+  // Remove the Browser from the browser list. The browser itself is still
+  // alive during this call, so any observer can act on it.
+  ProfileIOS* profile = browser->GetProfile();
+  BrowserList* browserList = BrowserListFactory::GetForProfile(profile);
+  browserList->RemoveBrowser(browser);
+
+  WebStateList* webStateList = browser->GetWebStateList();
+  // Close all webstates in `webStateList`. Do this in an @autoreleasepool as
+  // WebStateList observers will be notified (they are unregistered later). As
+  // some of them may be implemented in Objective-C and unregister themselves
+  // in their -dealloc method, ensure the -autorelease introduced by ARC are
+  // processed before the WebStateList destructor is called.
+  @autoreleasepool {
+    CloseAllWebStates(*webStateList, WebStateList::CLOSE_NO_FLAGS);
+  }
+}
+
+- (void)destroyAndRebuildIncognitoProfile {
+  DCHECK(_main_profile->HasOffTheRecordProfile());
+  _nonPersistentWebViewConfiguration = nil;
+
+  ProfileIOS* otrProfile = _main_profile->GetOffTheRecordProfile();
+
+  BrowsingDataRemover* browsingDataRemover =
+      BrowsingDataRemoverFactory::GetForProfile(otrProfile);
+  browsingDataRemover->Remove(browsing_data::TimePeriod::ALL_TIME,
+                              BrowsingDataRemoveMask::REMOVE_ALL,
+                              base::DoNothing());
+
+  [self cleanupBrowser:_otr_browser.get()];
+  _otr_browser.reset();
+
+  // Destroy and recreate the off-the-record BrowserState.
+  _main_profile->DestroyOffTheRecordProfile();
+
+  otrProfile = _main_profile->GetOffTheRecordProfile();
+  _otr_browser = Browser::Create(otrProfile, {});
+
+  BrowserList* browserList = BrowserListFactory::GetForProfile(otrProfile);
+  browserList->AddBrowser(_otr_browser.get());
+}
 
 @end